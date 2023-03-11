3bc61efaa22efddd29bf31f221887e6e4780b1a1
Sajjad Servatjoo <s.servatjoo@tejaratshayan.com>
2023-03-11 16:22:41 +0330

    update

--- namizun_menu/setup.py
+++ namizun_menu/setup.py
@@ -9,5 +9,5 @@ setup(name='namizun_menu',
       setup_requires=['wheel'],
       install_requires=['colored~=1.4.4',
                         'pyfiglet~=0.8.post1',
-                        'prettytable~=3.5.0']
+                        'prettytable']
       )
