d7456d133cf68e5a0233188c7e310a6199e0f329
Sajjad Servatjoo <s.servatjoo@tejaratshayan.com>
2023-03-11 16:03:50 +0330

    update

--- else/setup.sh
+++ else/setup.sh
@@ -1,67 +1,149 @@
 #!/usr/bin/env bash
 
-echo 'Install prerequisites (step 1)'
-apt update && apt install python3-pip python3-venv redis git -y
-if [ $? == 0 ]; then
-  echo 'Successfully installed'
-else
-  echo 'An error occurred while installing the prerequisites'
-  exit
-fi
+detect_os() {
+    # Detect OS
+    # $os_version variables aren't always in use, but are kept here for convenience
+    if grep -qs "ubuntu" /etc/os-release; then
+        os="ubuntu"
+        os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+    elif [[ -e /etc/debian_version ]]; then
+        os="debian"
+        os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
+    elif [[ -e /etc/almalinux-release || -e /etc/rocky-release || -e /etc/centos-release ]]; then
+        os="centos"
+        os_version=$(grep -shoE '[0-9]+' /etc/almalinux-release /etc/rocky-release /etc/centos-release | head -1)
+    elif [[ -e /etc/fedora-release ]]; then
+        os="fedora"
+        os_version=$(grep -oE '[0-9]+' /etc/fedora-release | head -1)
+    else
+        echo "This installer seems to be running on an unsupported distribution.
+    Supported distros are Ubuntu, Debian, AlmaLinux, Rocky Linux, CentOS and Fedora."
+        exit
+    fi
+}
+detect_os() {
+    # Detect OS
+    # $os_version variables aren't always in use, but are kept here for convenience
+    if grep -qs "ubuntu" /etc/os-release; then
+        os="ubuntu"
+        os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+    elif [[ -e /etc/debian_version ]]; then
+        os="debian"
+        os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
+    elif [[ -e /etc/almalinux-release || -e /etc/rocky-release || -e /etc/centos-release ]]; then
+        os="centos"
+        os_version=$(grep -shoE '[0-9]+' /etc/almalinux-release /etc/rocky-release /etc/centos-release | head -1)
+    elif [[ -e /etc/fedora-release ]]; then
+        os="fedora"
+        os_version=$(grep -oE '[0-9]+' /etc/fedora-release | head -1)
+    else
+        echo "This installer seems to be running on an unsupported distribution.
+    Supported distros are Ubuntu, Debian, AlmaLinux, Rocky Linux, CentOS and Fedora."
+        exit
+    fi
+}
+permissions() {
 
-echo 'start redis service'
-systemctl start redis.service
-if [ $? == 0 ]; then
-  echo 'redis servie started'
-else
-  echo 'service starting failed'
-  exit
-fi
+    # Detect environments where $PATH does not include the sbin directories
+    if ! grep -q sbin <<< "$PATH"; then
+        echo "$PATH does not include sbin. Try using "su -" instead of 'su'."
+        exit
+    fi
 
-echo "Creating namizun directory (step 2)"
-mkdir -p /var/www/namizun && cd /var/www/namizun
+    if [[ "$EUID" -ne 0 ]]; then
+        echo "This installer needs to be run with superuser privileges."
+        echo -e "Run the script as root until the installation is complete, after that use it with normal privileges."
+        exit
+    fi
+}
+install_dependencies() {
+    echo 'Install prerequisites (step 1)'
+    if ! [[ $(command -v "$i") ]] ; then
+        echo -e "Install PKG: $i"
+        permissions
 
-echo 'Pulling the repository (step 3)'
-git init
-git remote add origin https://github.com/malkemit/namizun.git
-git pull origin master
-if [ $? != 0 ]; then
-  echo 'could not clone the repository'
-  exit
-fi
+    #    echo -e "go not installed"
+        if [[ "$os" = "debian" ]]; then
 
-echo 'Create virtual env (step 4)'
-python3 -m venv /var/www/namizun/venv
-if [ $? != 0 ]; then
-  echo "VENV didn't created"
-fi
+            apt-get update
+            apt install -y python3-pip python3-venv redis git 
 
-echo 'Installing project dependencies (step 5)'
-cd /var/www/namizun && source /var/www/namizun/venv/bin/activate && pip install wheel && pip install namizun_core/ namizun_menu/ && deactivate
-if [ $? != 0 ]; then
-  echo "Dependencies doesn't installed correctly"
-  exit
-fi
+        elif [[ "$os" = "ubuntu" ]]; then
+            apt-get update
+            apt install -y python3-pip python3-venv redis git 
 
-echo 'Create namizun service (step 6)'
-ln -s /var/www/namizun/else/namizun.service /etc/systemd/system/
-if [ $? != 0 ]; then
-  echo 'Creating service was failed'
-  exit
-fi
+        elif [[ "$os" = "centos" ]]; then
+            yum install -y python3-pip python3-venv redis git 
+        else
+            # Else, OS must be Fedora
+            dnf install -y python3-pip python3-venv redis git 
+        fi
+    fi
+    if [ $? == 0 ]; then
+      echo 'Successfully installed'
+    else
+      echo 'An error occurred while installing the prerequisites'
+      exit
+    fi    
 
-echo 'Reload services and start namizun.service (step 7)'
-systemctl daemon-reload
-sudo systemctl enable namizun.service
-sudo systemctl start namizun.service
-if [ $? != 0 ]; then
-  echo "Namizun service didn't started"
-  exit
-fi
+    echo 'start redis service'
+    if command -v redis &> /dev/null; then
+        systemctl enable --now  redis.service
+    fi
 
-echo "make namizun as a command (step 8)"
-ln -s /var/www/namizun/else/namizun /usr/local/bin/ && chmod +x /usr/local/bin/namizun
-if [ $? != 0 ]; then
-  echo "failed to add namizun to PATH environment variables"
-  exit
-fi
+    echo "Creating namizun directory (step 2)"
+    mkdir -p /var/www/namizun && cd /var/www/namizun
+
+    echo 'Pulling the repository (step 3)'
+    git init
+    git remote add origin https://github.com/malkemit/namizun.git
+    git pull origin master
+    if [ $? != 0 ]; then
+      echo 'could not clone the repository'
+      exit
+    fi
+
+    echo 'Create virtual env (step 4)'
+    python3 -m venv /var/www/namizun/venv
+    if [ $? != 0 ]; then
+      echo "VENV didn't created"
+    fi
+
+    echo 'Installing project dependencies (step 5)'
+    cd /var/www/namizun && source /var/www/namizun/venv/bin/activate && pip install wheel && pip install namizun_core/ namizun_menu/ && deactivate
+    if [ $? != 0 ]; then
+      echo "Dependencies doesn't installed correctly"
+      exit
+    fi
+
+    echo 'Create namizun service (step 6)'
+    ln -s /var/www/namizun/else/namizun.service /etc/systemd/system/
+    if [ $? != 0 ]; then
+      echo 'Creating service was failed'
+      exit
+    fi
+
+    echo 'Reload services and start namizun.service (step 7)'
+    systemctl daemon-reload
+    sudo systemctl enable namizun.service
+    sudo systemctl start namizun.service
+    if [ $? != 0 ]; then
+      echo "Namizun service didn't started"
+      exit
+    fi
+
+    echo "make namizun as a command (step 8)"
+    ln -s /var/www/namizun/else/namizun /usr/local/bin/ && chmod +x /usr/local/bin/namizun
+    if [ $? != 0 ]; then
+      echo "failed to add namizun to PATH environment variables"
+      exit
+    fi
+
+}
+
+
+
+
+
+
+
